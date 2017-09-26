8f9898e04c260338763aa192b80029d71501faab
Sophie Kores <sophie@spoton.se>
2017-09-26 15:10:49 +0200

    Added Phaser and pre-loading the game

---
+++ source/maingamestate.js
@@ -0,0 +1,13 @@
+// Create an empty object
+var mainGameState = {}
+
+// Add the preload function
+mainGameState.preload = function() {
+    console.log("Pre-loading the Game");
+}
+
+// Add the create function
+mainGameState.create = function() {}
+
+// Add the update function
+mainGameState.update = function() {}
