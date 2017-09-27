4e44f6cfa307545184e9a4ecf55e959793cb36a3
Sophie Kores <sophie@spoton.se>
2017-09-27 15:19:03 +0200

    Spawn asteroids!
    Added asteroids and fired them every 2 second, varying in size and sprite.

--- source/maingamestate.js
+++ source/maingamestate.js
@@ -1,5 +1,7 @@
 // Constants
 const spaceShipSpeed = 250;
+const asteroidSpeedMin = 50;
+const asteroidSpeedMax = 200;
 
 // Create an empty object
 var mainGameState = { }
@@ -7,7 +9,10 @@ var mainGameState = { }
 mainGameState.preload = function() {
     console.log("Pre-loading the Game");
     this.game.load.image("space-bg", "assets/images/space-bg.jpg");
-    this.game.load.image("space-ship", "assets/images/player-ship.png");   
+    this.game.load.image("space-ship", "assets/images/player-ship.png");  
+    this.game.load.image("asteroid", "assets/images/asteroid-small-02.png");
+    this.game.load.image("asteroid-medium", "assets/images/asteroid-medium-01.png");
+    this.game.load.image("asteroid-small", "assets/images/asteroid-small-01.png");
     this.game.load.audio("space-music","assets/music/maingame.mp3");
 }
 
@@ -15,7 +20,6 @@ mainGameState.preload = function() {
 mainGameState.create = function() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.add.sprite(0,-300, 'space-bg');
-     
     
     // Place the space ship in the middle of the screen
     var midScreenX = game.width/2;
@@ -30,18 +34,33 @@ mainGameState.create = function() {
     this.spaceShip.anchor.set(0.5, 0.5);
     
     // Enable the player to move the space ship
-    game.physics.arcade.enable(this.spaceShip);        
+    game.physics.arcade.enable(this.spaceShip);
     
     // Adding some music    
     this.music = game.add.audio('space-music');
     this.music.play();
-    this.music.volume = 0.5;
+    this.music.volume = 0.01;
     this.music.loopFull();
+    
+    this.asteroidTimer = 2.0;
+    this.asteroids = game.add.group();
 }
 
 // Add the update function
 mainGameState.update = function() {
     mainGameState.updatePlayer();
+    this.asteroidTimer -= game.time.physicsElapsed;
+    
+    if(this.asteroidTimer <= 0.0) {
+        this.spawnAsteroids();
+        this.asteroidTimer = 2.0;
+    }
+   
+    for(var i = 0; i < this.asteroids.children.length; i++) {
+        if(this.asteroids.children[i].position.y > (game.height + 100)) {
+            this.asteroids.children[i].destroy();
+        }
+    }    
 }
 
 mainGameState.updatePlayer = function() {
@@ -69,15 +88,14 @@ mainGameState.updatePlayer = function() {
     }
     else {        
         this.spaceShip.body.velocity.y = 0;
-    }
-    
+    }    
     
     // Stop the space ship before it gets outside the screen (left)
     if (this.spaceShip.position.x < (spaceShipWidth/2) && this.cursors.left.isDown) {
         this.spaceShip.body.velocity.x = 0;
     }
     // Stop the space ship before it gets outside the screen (right)
-    if (this.spaceShip.position.x > (game.width-spaceShipWidth/2) &&                          this.cursors.right.isDown) {
+    if (this.spaceShip.position.x > (game.width-spaceShipWidth/2) && this.cursors.right.isDown) {
         this.spaceShip.body.velocity.x = 0;
     }
     
@@ -90,5 +108,27 @@ mainGameState.updatePlayer = function() {
     if (this.spaceShip.position.y < (game.height/2 + spaceShipHeight) && this.cursors.up.isDown) {
         this.spaceShip.body.velocity.y = 0;
     }
+    
+}
+
+mainGameState.spawnAsteroids = function() {
+    var asteroidSelection = ['asteroid', 'asteroid-medium', 'asteroid-small'];
+    var randomAsteroid = Math.floor(Math.random() * 3);    
+    var randomSize = Math.random() + 0.5;
+    var randomX = game.rnd.integerInRange(0, game.width);
+    var asteroidSpeed = Math.floor(Math.random() * (asteroidSpeedMax - asteroidSpeedMin) + asteroidSpeedMin);
+    var asteroidRotationSpeed = Math.floor(Math.random() * (asteroidSpeedMax - asteroidSpeedMin) + asteroidSpeedMin);
+    
+    var asteroid = game.add.sprite(randomX, 0, asteroidSelection[randomAsteroid]);
+    
+    asteroid.anchor.setTo(0.5, 0.5);
+    asteroid.scale.setTo(0.5,0.5);
+    game.physics.arcade.enable(asteroid);
+    asteroid.body.velocity.y = asteroidSpeed;
+    asteroid.body.angularVelocity = asteroidRotationSpeed;
+    this.asteroids.add(asteroid);
+}
+
+mainGameStat.spawnBullets = function() {
     
 }
